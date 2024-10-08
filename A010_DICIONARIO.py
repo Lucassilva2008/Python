194506ec609089761ab1e47e0c7e395ff6d6dc93
Lucassilva2008 <lucs200813@gmail.com>
2024-10-08 08:36:36 -0300

    .

---
+++ A010_DICIONARIO.py
@@ -0,0 +1,22 @@
+# DICIONARIO
+# dicionario = {chave:valor}
+# dicionario = dint(chave:valor)
+
+dicionario = {}
+
+dicionario ["Cidade"] = "São Paulo"
+dicionario["Estado"] = "SP"
+dicionario["Região"] = "Sudeste"
+
+print(dicionario)
+print(dicionario["Cidade"])
+print(dicionario.get("Estado"))
+print(dicionario.get("Chaveinexistente", "Valor alternativo se chave não existir"))
+
+
+#frutas = {
+    #"Laranja; R$5,00", 
+    #"Pera: R$3,00"
+#}
+
+#print(frutas)
